d55b4f5c4856c021479bc6189ea1dac4c8683af1
thomaschang <thomaschang@inquartik.com>
2019-02-26 00:15:15 +0800

    Make it to Vue.js step 0
    先看一下我們原本是怎麼用 context dictionary 跟 html 溝通的

--- judian/judian/app/views.py
+++ judian/judian/app/views.py
@@ -1,6 +1,7 @@
 """
 Definition of views.
 """
+import random
 from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpRequest
@@ -9,13 +10,18 @@ from django.http import HttpRequest
 def home(request):
     """Renders the home page."""
     assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/index.html',
-        {
-            'title':'Home Page',
-            'year':datetime.now().year,
-        }
+    context = {}
+    context['title'] = 'Home Page'
+    context['year'] = datetime.now().year
+    #return render(
+    #    request,
+    #    'app/index.html',
+    #    {
+    #        'title':'Home Page',
+    #        'year':datetime.now().year,
+    #    }
+    #)
+    return render(request, 'app/index.html', context
     )
 
 def contact(request):
@@ -34,12 +40,27 @@ def contact(request):
 def about(request):
     """Renders the about page."""
     assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/about.html',
-        {
-            'title':'About',
-            'message':'Your application description page.',
-            'year':datetime.now().year,
-        }
-    )
+    #return render(
+    #    request,
+    #    'app/about.html',
+    #    {
+    #        'title':'About',
+    #        'message':'Your application description page.',
+    #        'year':datetime.now().year,
+    #    }
+    #)
+    context = {}
+    context['title'] = 'About'
+    context['message'] = 'Your application description page.'
+    context['year'] = datetime.now().year
+    # add some dummy data
+    names = ("bob", "dan", "jack", "lizzy", "susan")
+    items = []
+    for i in range(10):
+        items.append({
+            "name": random.choice(names),
+            "age": random.randint(20,80),
+            "url": "https://example.com",
+        })
+    context["items"] = items
+    return render(request, 'app/about.html', context)
