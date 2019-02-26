0faf51d411165dde1a1a5b8939767771af010c54
thomaschang <thomaschang@inquartik.com>
2019-02-26 20:21:34 +0800

    Make it to Vue.js step 4
    新增 v-model 及 component 的範例

--- judian/judian/app/static/app/scripts/vue_about.js
+++ judian/judian/app/static/app/scripts/vue_about.js
@@ -1,3 +1,12 @@
+Vue.component('todo-item-simple', {
+    template: '<li>This is a todo</li>',
+})
+
+Vue.component('grocery-item', {
+    props: ['gc'],
+    template: '<li>{{ gc.text }}</li>'
+})
+
 var app = new Vue({
     delimiters: ['[[', ']]'],
     el: '#app',
@@ -8,11 +17,19 @@ var app = new Vue({
             { text: 'Learn JavaScript' },
             { text: 'Learn Vue' },
             { text: 'Build something awesome' }
+        ],
+        groceryList: [
+            { id: 0, text: 'Vegetables' },
+            { id: 1, text: 'Cheese' },
+            { id: 2, text: 'Whatever else humans are supposed to eat' }
         ]
     },
     methods: {
         greet: function (name) {
             console.log('Hello from ' + name + '!')
+        },
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
         }
     }
 });
